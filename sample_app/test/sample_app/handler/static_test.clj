9ac909c0dee61499c21b68fa03e849e2cd3b13bd
sandmark <sandmark.m@gmail.com>
2020-06-16 05:08:52 +0900

    Improve response test with kerodon instead of mocking

--- sample_app/test/sample_app/handler/static_test.clj
+++ sample_app/test/sample_app/handler/static_test.clj
@@ -1,38 +1,23 @@
 (ns sample-app.handler.static-test
-  (:require [ataraxy.response :as response]
-            [clojure.test :as t]
-            [integrant.core :as ig]
-            [ring.mock.request :as mock]
-            [sample-app.handler.static :as sut]))
+  (:require [clojure.test :as t]
+            [kerodon.core :as k]
+            [kerodon.test :as kt]
+            [sample-app.test-helper.core :as helper :refer [with-system]]))
 
-(defn request
-  ([handler-key uri opts]
-   (let [{:keys [params method key location]} (merge {:params   {}
-                                                      :method   :get
-                                                      :key      handler-key
-                                                      :location uri}
-                                                     opts)
-         handler                              (ig/init-key key params)]
-     (handler (mock/request method location))))
+(t/deftest static-pages-with-kerodon-test
+  (with-system [system (helper/test-system)]
+    (let [routes (:duct.router/ataraxy system)]
+      (t/testing "should get home"
+        (-> (k/session routes)
+            (k/visit "/static_pages/home")
+            (kt/has (kt/status? 200))))
 
-  ([handler-key uri]
-   (request handler-key uri {})))
+      (t/testing "should get help"
+        (-> (k/session routes)
+            (k/visit "/static_pages/help")
+            (kt/has (kt/status? 200))))
 
-(t/deftest static-pages-test
-  (t/testing "should get home"
-    (let [[response body] (request :sample-app.handler.static/home "/static_pages/home")]
-      (t/is (= ::response/ok response))
-      (t/is (= "Sample App"
-               (re-find #"Sample App" body)))))
-
-  (t/testing "should get help"
-    (let [[response body] (request :sample-app.handler.static/help "/static_pages/help")]
-      (t/is (= ::response/ok response))
-      (t/is (= "Get help"
-               (re-find #"Get help" body)))))
-
-  (t/testing "should get about"
-    (let [[response body] (request :sample-app.handler.static/about "/static_pages/about")]
-      (t/is (= ::response/ok response))
-      (t/is (= "This is the sample"
-               (re-find #"This is the sample" body))))))
+      (t/testing "should get about"
+        (-> (k/session routes)
+            (k/visit "/static_pages/about")
+            (kt/has (kt/status? 200)))))))
---
+++ sample_app/test/sample_app/test_helper/core.clj
@@ -0,0 +1,17 @@
+(ns sample-app.test-helper.core
+  (:require [clojure.java.io :as io]
+            [duct.core :as duct]
+            [integrant.core :as ig]))
+
+(duct/load-hierarchy)
+
+(defn test-system []
+  (-> (io/resource "sample_app/config.edn")
+      duct/read-config
+      (duct/prep-config [:duct.profile/dev :duct.profile/test])))
+
+(defmacro with-system [[bound-var binding-expr] & body]
+  `(let [~bound-var (ig/init ~binding-expr)]
+     (try
+       ~@body
+       (finally (ig/halt! ~bound-var)))))
