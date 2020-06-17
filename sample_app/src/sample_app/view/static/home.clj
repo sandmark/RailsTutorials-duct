0351444c0475ec8df79706e95cfb00f9fdf0bc9e
sandmark <sandmark.m@gmail.com>
2020-06-18 02:35:23 +0900

    Improve layout page

--- sample_app/src/sample_app/view/static/home.clj
+++ sample_app/src/sample_app/view/static/home.clj
@@ -1,11 +1,17 @@
 (ns sample-app.view.static.home
-  (:require  [sample-app.view.template :refer [page]]))
+  (:require [hiccup.element :refer [link-to image]]
+            [sample-app.view.template :refer [page]]))
 
 (defn render-home []
   (page
    {}
-   [:h1 "Sample App"]
-   [:p
-    "This is the home page for the "
-    [:a {:href "https://railstutorial.jp/"} "Ruby on Rails Tutorial"]
-    " sample application."]))
+   [:div.center.jumbotron
+    [:h1 "Welcome to the Sample App"]
+    [:h2
+     "This is the home page for the "
+     (link-to "https://railstutorial.jp/" "Ruby on Rails Tutorial")
+     " sample application."]
+
+    (link-to {:class "btn btn-lg btn-primary"} "#" "Sign up now!")]
+   (link-to "http://rubyonrails.org/"
+            (image {:alt "Rails logo"} "/images/rails.png"))))
--- sample_app/src/sample_app/view/template.clj
+++ sample_app/src/sample_app/view/template.clj
@@ -1,5 +1,6 @@
 (ns sample-app.view.template
-  (:require [hiccup.page :refer [html5]]))
+  (:require [hiccup.element :refer [link-to]]
+            [hiccup.page :refer [html5 include-css include-js]]))
 
 (defn- full-title [title]
   (cond->> "Ruby on Rails Tutorial Sample App"
@@ -8,5 +9,28 @@
 (defn page [{:keys [title]} & contents]
   (html5
    [:head
-    [:title (full-title title)]]
-   [:body contents]))
+    [:title (full-title title)]
+    (include-css "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+                 "/css/style.css")
+    "<!--[If lt IE 9]>"
+    (include-js "https://cdnjs.cloudflare.com/ajax/libs/html5shiv/r29/html5.min.js")
+    "<![endif]-->"]
+   [:body
+    [:header.navbar.navbar-fixed-top.navbar-inverse
+     [:div.container
+      (link-to {:id "logo"} "#" "sample app")
+      [:nav
+       [:ul.nav.navbar-nav.navbar-right
+        [:li (link-to "#" "Home")]
+        [:li (link-to "#" "Help")]
+        [:li (link-to "#" "Log in")]]]]]
+    [:div.container contents]
+    [:footer.footer
+     [:small
+      "The " (link-to "https://railstutorial.jp" "Ruby on Rails Tutorial") " by "
+      (link-to "http://www.michaelhartl.com/" "Michael Hartl")]
+     [:nav
+      [:ul
+       [:li (link-to "#" "About")]
+       [:li (link-to "#" "Contact")]
+       [:li (link-to "http://news.railstutorial.org/" "News")]]]]]))
