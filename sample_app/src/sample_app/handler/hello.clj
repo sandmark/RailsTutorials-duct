195e4d0b6e1d5b2a05f2a34d4c90d9bd31ef64b5
sandmark <sandmark.m@gmail.com>
2020-06-18 20:06:32 +0900

    Remove unnecessary file: hello handler

--- sample_app/src/sample_app/handler/hello.clj
+++
@@ -1,7 +0,0 @@
-(ns sample-app.handler.hello
-  (:require [ataraxy.response :as response]
-            [integrant.core :as ig]))
-
-(defmethod ig/init-key :handler/hello [_ _]
-  (fn [_]
-    [::response/ok "hello, world!"]))
