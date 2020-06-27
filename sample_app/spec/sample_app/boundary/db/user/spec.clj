5c6c0891a320d9f75432ea7b449c1436496b81fc
sandmark <sandmark.m@gmail.com>
2020-06-27 13:40:42 +0900

    Add specs for functions

--- sample_app/spec/sample_app/boundary/db/user/spec.clj
+++ sample_app/spec/sample_app/boundary/db/user/spec.clj
@@ -1,5 +1,7 @@
 (ns sample-app.boundary.db.user.spec
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s]
+            [sample-app.boundary.db.core.spec :as db]
+            [sample-app.boundary.db.user :as sut]))
 
 (s/def ::name string?)
 (s/def ::email string?)
@@ -7,3 +9,13 @@
 (s/def ::updated_at #(instance? org.joda.time.DateTime %))
 (s/def ::user (s/keys :req-un [::name ::email]
                       :opt-un [::created_at ::updated_at]))
+
+(s/fdef sut/get-user-by-id
+  :args (s/cat :db ::db/db
+               :id integer?)
+  :ret (s/nilable ::db/row-map))
+
+(s/fdef sut/create-user
+  :args (s/cat :db ::db/db
+               :user ::user)
+  :ret (s/nilable ::db/row-id))
