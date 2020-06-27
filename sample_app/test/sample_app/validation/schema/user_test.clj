64b301726e2dc45325480cd92e2bc990245a6aa8
sandmark <sandmark.m@gmail.com>
2020-06-27 13:45:50 +0900

    Add validation: email should be present

--- sample_app/test/sample_app/validation/schema/user_test.clj
+++ sample_app/test/sample_app/validation/schema/user_test.clj
@@ -1,15 +1,14 @@
 (ns sample-app.validation.schema.user-test
   (:require [sample-app.validation.schema.user :as sut]
-            [clojure.test :as t]
-            [struct.core :as struct]))
-
-(defn valid-user? [user]
-  (struct/valid? user sut/validator))
+            [clojure.test :as t]))
 
 (t/deftest validation-test
   (let [user {:name "Example User" :email "user@example.com"}]
     (t/testing "should be valid"
-      (t/is (valid-user? user)))
+      (t/is (sut/valid-user? user)))
 
     (t/testing "name should be present"
-      (t/is (not (valid-user? (assoc user :name "     ")))))))
+      (t/is (not (sut/valid-user? (assoc user :name "     ")))))
+
+    (t/testing "email should be present"
+      (t/is (not (sut/valid-user? (assoc user :email "    ")))))))
